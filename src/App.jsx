d60032c57f860a84ca2c91b0f6cbb4b698adb320
NotVinnny <vh222@cornell.edu>
2023-07-20 00:06:41 -0400

    Added useState to remember value

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-function Square({ value }) {
+import { useState } from 'react';
+
+function Square() {
+  const [value, setValue] = useState(null); // value stores valu and setValue is function used to change value
+  // null passed to useState is the initial value for state variable so value is equal to null
   function handleClick() {
-    console.log("clicked!");
+    setValue('X');
   }
 
   return (
@@ -17,19 +21,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
